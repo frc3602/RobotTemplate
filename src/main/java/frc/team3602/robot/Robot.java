0dc5858864cd95405dcd056acdececd898c672d3
Cody L. Wellman <cody@codexmicro.systems>
2023-01-04 11:23:00 -0500

    Added all files

---
+++ src/main/java/frc/team3602/robot/Robot.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 Team 3602 All rights reserved. This work is
+ * licensed under the terms of the MIT license which can be found in the
+ * root directory of this project.
+ */
+
+package frc.team3602.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private RobotContainer robotContainer = new RobotContainer();
+
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+}
